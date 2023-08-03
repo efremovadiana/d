7dbf58c37335c08db5d2cc78c7f52f46ee920339
efremovadiana <136587564+efremovadiana@users.noreply.github.com>
2023-08-03 23:16:03 +0600

    Add files via upload

--- app_advertisment/admin.py
+++ app_advertisment/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Advertisement
+
+# импорт классов для подсказок
+from django.db.models.query import QuerySet
 
 # Register your models here.
+
+class AdvertisementsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'title', 'price', 'user', 'auction', 'created_date', 'created_at', 'update_at']
+    list_filter = ['auction', 'price', 'created_at']
+    actions = ['make_auction_as_false', 'make_auction_as_true'] # указываем функции
+    fieldsets = (
+        ( # Первый блок
+            'Общее', # название блока
+            {
+                "fields": ('title', 'text')
+            }
+        ),
+        ( # Второй блок
+            'Финансы', # название блока
+            {
+                "fields": ('price', 'auction'),
+                'classes': ['collapse'] # для скрытия
+            }
+        )
+    )
+
+    # создаём функции
+    @admin.action(description='Убрать возможность торга') # менять у записей возможность торга на False
+    def make_auction_as_false(self, request, queryset: QuerySet):
+        queryset.update(auction=False)
+
+    
+    @admin.action(description='Добавить возможность торга') # менять у записей возможность торга на False
+    def make_auction_as_true(self, request, queryset: QuerySet):
+        queryset.update(auction=True)
+
+
+
+
+
+
+
+
+
+# указали модель и админ класс (для кастомизации)
+admin.site.register(Advertisement, AdvertisementsAdmin)
--- app_advertisment/models.py
+++ app_advertisment/models.py
@@ -1,10 +1,46 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 
-#class Advertisment(models.Model):
- #   title = models.CharField('заголовок', max_length = 128)
-  #  text = models.TextField('текст')
-   # price = models.FloatField('цена')
-    #user = models.CharField("пользователь", max_length=126) # пользователь (пока что просто имя)
-    #date = models.DateField("дата", auto_now_add = True)
+class Advertisement(models.Model):
+    title = models.CharField("заголовок", max_length=128)
+    text = models.TextField("текст")
+    price = models.FloatField("цена")
+    user = models.CharField("пользователь", max_length=126) # пользователь (пока что просто имя) 
+    #date = models.DateField("дата", auto_now_add=True)
+    auction = models.BooleanField("торг", help_text="Возможен торг или нет", default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    # Представление в виде строки
+    def __str__(self):
+      return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    # настройки для таблицы
+    class Meta:
+      db_table = 'advertisments' # переименовали таблицу
+
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+      from django.utils import timezone
+      from django.utils.html import format_html
+      
+      if self.created_at.date() == timezone.now().date():
+        create_time = self.created_at.time().strftime('%H:%M:%S')
+        return format_html(
+         '<span style = "color:green; font-weight = bold;">Сегодня в {}</span>',create_time
+
+        )
+      return self.created_at.strftime('%d.%m.%Y at %H:%M:%S')
+      print(help(created_at))
+
+
+    #
+    def __str__(self):
+      return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+
+    class Meta:
+      db_table = 'advertisments' # переименовали таблицу
